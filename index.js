24355be4558af2b0c1913b06a38620a1f4d47f37
sgil <gilljosara@gmail.co>
2016-02-07 14:18:30 +0100

    trying ejs

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
-var hej = require('cool-ascii-faces');
-var express = require('express');
+//var hej = require('cool-ascii-faces');
+var express = require('express'); 
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('index',{user: "Great User",title:"homepage"});
 });
 
-app.get('/hej', function(request, response) {
-	response.send(hej());
-});
+//app.get('/hej', function(request, response) {
+	//response.send(hej());
+//});
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+//app.listen(app.get('port'), function() {
+  //console.log('Node app is running on port', app.get('port'));
+//});
 
 
