d0169636484b1976f576b60712bde1ea48b81829
sgil <gilljosara@gmail.co>
2016-02-07 14:44:54 +0100

    revert

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 var hej = require('cool-ascii-faces');
-var express = require('express'); 
+var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('index',{user: "Great User",title:"homepage"});
+  response.render('pages/index');
 });
 
 app.get('/hej', function(request, response) {
@@ -15,5 +21,3 @@ app.get('/hej', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
