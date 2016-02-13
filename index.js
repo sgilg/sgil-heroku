09e515b7cb832c4dc77ca4de4ebbeb2a13411a1f
sgil <gilljosara@gmail.co>
2016-02-13 11:29:53 +0100

    adding cloudinary basics

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var hej = require('cool-ascii-faces');
+var cloudinary = require('cludinary');
 var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
+
+// Configure Cloudinary access
+cloudinary.config({ 
+  cloud_name: 'sgil', 
+  api_key: '784949991342827', 
+  api_secret: 'JQDfPxlF5F2aUYunN2Zc0eAp8_M' 
+});
 
 app.use(express.static(__dirname + '/public'));
 
